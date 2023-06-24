707ee2f156c0f8ecce0fa505cd5340024e54dcc0
FartGarfild <40548064+FartGarfild@users.noreply.github.com>
2023-06-24 19:17:28 +0300

    Update bk.sh
    Фикс багов и оптимизация

--- bk.sh
+++ bk.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
+#Проверка на наличие одной из четырёх панелей управления, если нет ниодной скрипт останавливается
 if [ ! -d "/etc/VestaCP" ] && [ ! -d "/etc/HestiaCP" ] && [ ! -d "/etc/cyberpanel" ] && [ ! -d "/usr/local/mgr5" ]; then
 exit 1
 fi
+#Проверка на наличие папки бекап, если нет создаст.
 if [ ! -d "/backup" ]; then
-mkdir -p "$FOLDER_PATH"
+mkdir -p "/backup"
 fi
-cd /backup
 mkdir /backup/tmp.bk
 mkdir /backup/tmp.bk/web
 mkdir /backup/tmp.bk/db
@@ -15,13 +16,13 @@ DB_HOST="localhost"
 DB_USER="root" #Пользователь баз данных
 DB_PASSWORD="" #Пароль от пользователя 
 
-#From FTP
+#Настройки подключения FTP
+SERVER="" #Сервер FTP
 USER="" #Пользователь FTP
 PASS="" #Пароль FTP
-SERVER="" #Сервер FTP
 WHERE2=""#Путь в FTP хранилище куда будут идти файлы
 
-
+#Проверка баз данных и исключение стандартных БД.
 DB_LIST=$(mysql -h $DB_HOST -u $DB_USER -p$DB_PASSWORD -e "SHOW DATABASES WHERE \`Database\` NOT IN ('information_schema', 'mysql', 'performance_schema', 'sys');" | grep -v Database)
 
 # Имя и путь для сохранения резервных копий
@@ -34,29 +35,25 @@ done
 # Проверка наличия папки VestaCP в директории /etc
 if [ -d "/etc/VestaCP" ]; then
     echo "Найдена папка VestaCP в директории /etc"
-    # Ваши действия для VestaCP
 	rsync -azhP /home/admin/web* /backup/tmp.bk/web
-fi
+ fi
 # Проверка наличия папки HestiaCP в директории /etc
 if [ -d "/etc/HestiaCP" ]; then
     echo "Найдена папка HestiaCP в директории /etc"
-    # Ваши действия для HestiaCP
 	rsync -azhP /home/admin/web* /backup/tmp.bk/web
 fi
 if [ -d "/etc/cyberpanel" ]; then
-    echo "Найдена папка CyberPanel в директории /etc"
-    # Ваши действия для HestiaCP
+    echo "Найдена папка CyberPanel в директории /etc" 
 	rsync -azhP /home/* /backup/tmp.bk/web
 fi
 # Проверка наличия папки mgr5 в директории /usr/local
 if [ -d "/usr/local/mgr5" ]; then
     echo "Найдена папка mgr5 в директории /usr/local"
-    # Ваши действия для ISPManager
 	rsync -azhP /var/www/www-root/data/www/* /backup/tmp.bk/web
 fi
 
 tar -cf /backup_$(date +%Y-%m-%d).tar.gz /backup/tmp.bk
 curlftpfs -o allow_other $USER:$PASS@SERVER:21 /mnt
-rsync -azhP /backup/*.tar.gz /$WHERE2
+cp /backup/*.tar.gz /$WHERE2
 unmount /mnt
 rm -rf /backup/tmp.bk
